7ec6369f8892a9f6ffed39a23c99061c12aedce9
liuchang <37615085@qq.com>
2017-12-21 13:08:50 +0800

    modify app.py

--- news/app.py
+++ news/app.py
@@ -1,11 +1,39 @@
 #!/usr/bin/env python3
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask import render_template,abort
 import os
 import json
 
 app=Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD']=True
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root@localhost/Blog'
+db=SQLAlchemy(app)
+
+class File(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(80),unique=True)
+    created_time=db.Column(db.DateTime,unique=False)
+    category_id=db.Column(db.Integer,unique=True,db.ForeignKey('category.id'))
+    content=db.Column(db.Text,unique=False)
+
+    def __init__(self,title,content):
+        self.title=title
+        self.content=content
+
+    def __repr__(self):
+        return '<File %r>' % self.title
+
+
+class Category(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(80),unique=True)
+
+    def __init__(self,name):
+        self.name=name
+
+    def __repr__(self):
+        return '<Category %r>' % self.name
 
 path="/home/shiyanlou/files/"
 pages=os.listdir(path)

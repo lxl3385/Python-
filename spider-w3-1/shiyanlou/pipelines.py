277019e3e0149b146219b22e11c1dde9df3f3f5d
liuchang <37615085@qq.com>
2018-01-02 03:57:31 +0800

    modify file

--- spider-w3-1/shiyanlou/pipelines.py
+++ spider-w3-1/shiyanlou/pipelines.py
@@ -7,18 +7,13 @@
 
 from datetime import datetime
 from sqlalchemy.orm import sessionmaker
-from shiyanlou.models import Repository,engine
-from shiyanlou.items import RepositoryItem 
+from shiyanlou.models import Repository,engine 
 
 class ShiyanlouPipeline(object):
     def process_item(self, item, spider):
-        self._process_repository_item(item)
-        return item
-
-
-    def _process_repository_item(self,item):
         item['update_time']=datetime.strptime(item['update_time'],'%Y-%m-%dT%H:%M:%SZ')
         self.session.add(Repository(**item))
+        return item
 
     def open_spider(self,spider):
         Session=sessionmaker(bind=engine)
--- spider-w3-1/shiyanlou/spiders/repositories.py
+++ spider-w3-1/shiyanlou/spiders/repositories.py
@@ -5,14 +5,20 @@ from shiyanlou.items import RepositoryItem
 
 class RepositoriesSpider(scrapy.Spider):
     name = 'repositories'
+    allowed_domains=['github.com']
+
     @property
     def start_urls (self):
-        url_tmpl= 'https://github.com/shiyanlou?tab=repositories&page{}'
+        url_tmpl= 'https://github.com/shiyanlou?page{}&tab=repositories'
         return (url_tmpl.format(i) for i in range(1,5))
 
     def parse(self, response):
-        for course in response.css('li.py-4'):
-            yield RepositoryItem({
-                'name':course.css('a::text').re_first("\n\s*(.*)"),
-                'update_time':course.css('relative-time::attr(datetime)').extract_first()
+        for repository in response.css('li.public'):
+            item= RepositoryItem({
+                'name':repository.xpath('.//a[@itemprop="name codeRepository"]/text()').re_first("\n\s*(.*)"),
+                'update_time':repository.xpath('.//relative-time/@datetime').extract_first()
             })
+            yield item
+
+#'name':course.css('a::text').re_first("\n\s*(.*)"),
+#'update_time':course.css('relative-time::attr(datetime)').extract_first()

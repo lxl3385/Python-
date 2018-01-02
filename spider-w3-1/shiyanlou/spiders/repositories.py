a17cca5eea4e77caa09b15e80ae279ee4e95e039
lxl3385 <liuchang3385@gmail.com>
2018-01-02 00:09:34 -0500

    Update repositories.py

--- spider-w3-1/shiyanlou/spiders/repositories.py
+++ spider-w3-1/shiyanlou/spiders/repositories.py
@@ -9,7 +9,7 @@ class RepositoriesSpider(scrapy.Spider):
 
     @property
     def start_urls (self):
-        url_tmpl= 'https://github.com/shiyanlou?page{}&tab=repositories'
+        url_tmpl= 'https://github.com/shiyanlou?page={}&tab=repositories'
         return (url_tmpl.format(i) for i in range(1,5))
 
     def parse(self, response):
